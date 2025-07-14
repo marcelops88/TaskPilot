7da6d8b150be58aa8346d61e98362c76ea2f79b2
Marcelo Santana <marcellopscv2017@gmail.com>
2025-07-14 16:16:21 -0300

    feat: documentação usando o Swashbuckle

--- TaskPilot.API/Program.cs
+++ TaskPilot.API/Program.cs
@@ -1,14 +1,30 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi(); 
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TaskPilot API",
+        Version = "v1",
+        Description = "TaskPilot é uma API moderna e intuitiva para gerenciamento de tarefas, criada para ajudar equipes a organizar, monitorar e colaborar em suas atividades diárias."
+    });
+
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskPilot API V1");
+    });
 }
 
 app.UseHttpsRedirection();
